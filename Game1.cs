a4fc8ab93f79e5af81c61687ff3cc3e3eea54a95
FranciscoRibeiroSilva <franciscoribeirsil@gmail.com>
2025-11-24 12:59:09 -0300

    fix centered the logo

--- Game1.cs
+++ Game1.cs
@@ -41,7 +41,19 @@ public class Game1 : Core
 
         // TODO: Add your drawing code here
         SpriteBatch.Begin();
-        SpriteBatch.Draw(_logo, Vector2.Zero, Color.White);
+         SpriteBatch.Draw(
+        _logo,              // texture
+        new Vector2(        // position
+            (Window.ClientBounds.Width * 0.5f) - (_logo.Width * 0.5f),
+            (Window.ClientBounds.Height * 0.5f) - (_logo.Height * 0.5f)),
+        null,               // sourceRectangle
+        Color.White,        // color
+        0.0f,               // rotation
+        Vector2.Zero,       // origin
+        1.0f,               // scale
+        SpriteEffects.None, // effects
+        0.0f                // layerDepth
+        );
         SpriteBatch.End();
 
         base.Draw(gameTime);
